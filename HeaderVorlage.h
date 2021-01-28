04db7411ab174f76f434d73b1f830ecbc6d78635
Georg <48410861+weltbester@users.noreply.github.com>
2021-01-28 15:52:59 +0100

    Initial commit

---
+++ HeaderVorlage.h
@@ -0,0 +1,29 @@
+/** 
+ * File: filename.h
+ * Author: Georg Pohl
+ * Date of creation: 1st of January 2021
+ * Version: 1.0
+ * Last change: 1st of January 2021
+ * -------------------------------------
+ * Description:
+ *  Insert description here...
+ *  ...
+ *
+ * Copyright (C) Jan. 2021: Georg Pohl, 70174 Stuttgart
+ */
+#ifndef FILENAME_H_
+#define FILENAME_H_
+
+// no absolute path entries
+#include <stdio.h>
+
+
+// Typaliase (typedef)
+
+// Constants
+
+// Makros, Declarations, ...
+  int foo(int x);
+
+#endif */ FILENAME_H_ */
+/* End of filename.h */
---
+++ MainVorlage.c
@@ -0,0 +1,44 @@
+/** 
+ * File: filename.c
+ * Author: Georg Pohl
+ * Date of creation: 1st of January 2021
+ * Version: 1.0
+ * Last change: 1st of January 2021
+ * -------------------------------------
+ * Description:
+ *  Insert description here...
+ *  ...
+ *
+ * Copyright (C) Jan. 2021: Georg Pohl, 70174 Stuttgart
+ */
+
+#ifdef MACOS
+Commands ...
+#endif /* MACOS */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* CONSTANTS */
+
+/* STRUCTS */
+class human : public std::string {
+    
+  // Default construktor sufficient
+    
+  Methods ...
+}
+
+/* PROTOTYPES */
+
+
+int main(int argc, const char **argv) {
+  /* !TODO: gpohl 2021-01-01
+  Put code in here... */
+
+  return EXIT_SUCCESS;
+} /* filename.c */
+
+/* FUNCTIONS */
+
+/* End of filename.c */
